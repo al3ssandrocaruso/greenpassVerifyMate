efba05ba60628cdf4ec5ce314d374fff4ea5a9ac
Alessandro Caruso <alessandro.caruso@students.uniroma2.eu>
2022-02-02 12:42:14 +0100

    Initial commit

---
+++ greenpassVerifyMate/src/main/java/verify/ApiCertificate.java
@@ -0,0 +1,18 @@
+package verify;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+import java.util.List;
+
+public interface ApiCertificate {
+
+
+    @GET("signercertificate/update")
+    Call<String> getCertUpdate(@Header("x-resume-token") String token);
+
+    @GET("signercertificate/status")
+    Call<List<String>> getCertStatus();
+
+}
